f504f48ddf564a950ee2e500e3b9104b2e11f3c7
Ryan Clarian <rclarian@gmail.com>
2024-07-09 09:42:19 +0800

    Database injection
    Database injection

--- API/CodePulse.API/CodePulse.API/Program.cs
+++ API/CodePulse.API/CodePulse.API/Program.cs
@@ -1,3 +1,6 @@
+using CodePulse.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DB injection 
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
 
 var app = builder.Build();
 
