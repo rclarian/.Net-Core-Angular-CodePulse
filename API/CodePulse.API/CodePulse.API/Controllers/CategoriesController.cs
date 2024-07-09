21b6c877620aca45635f7465a3e164565655108d
Ryan Clarian <rclarian@gmail.com>
2024-07-09 10:06:23 +0800

    Create Categories Controller
    Create Categories Controller

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    //https://localhost:xxxx/api/categories
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
