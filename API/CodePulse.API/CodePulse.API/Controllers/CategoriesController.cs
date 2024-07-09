4b5eceaf65255157991d02b1cdf965a964549253
Ryan Clarian <rclarian@gmail.com>
2024-07-09 11:12:33 +0800

    Create CreateCategory and save to db
    CreateCategory

--- API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
 {
     //https://localhost:xxxx/api/categories
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            //Map DTO to Domain model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await _dbContext.Categories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+
+            //Map domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle= category.UrlHandle
+            };
+
+            return Ok(response);
+        }
     }
 }
