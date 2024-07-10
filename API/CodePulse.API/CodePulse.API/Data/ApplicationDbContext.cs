952160d02d36f972bf9741b1070e0486f4087ef7
Ryan Clarian <rclarian@gmail.com>
2024-07-10 21:52:24 +0800

    #21. Create DbContext Class
    #21. Create DbContext Class

--- API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
+++ API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CodePulse.API.Data
 {
+    //#21. Create DbContext Class
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
