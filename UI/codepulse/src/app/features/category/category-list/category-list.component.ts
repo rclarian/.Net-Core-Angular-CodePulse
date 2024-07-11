fbf7e86c30afdeaaa9a39214dd61527b755fe6cd
Ryan Clarian <rclarian@gmail.com>
2024-07-11 12:52:12 +0800

    #44.Use Get all Categories Service in Component
    #44.Use Get all Categories Service in Component

--- UI/codepulse/src/app/features/category/category-list/category-list.component.ts
+++ UI/codepulse/src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { response } from 'express';
+import { error } from 'console';
+import { Subscription } from 'rxjs';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit, OnDestroy{
+
+  categories?: Category[];
+  private getCategorySubscription?: Subscription;
+  constructor(private categoryService: CategoryService){
+
+  }
+  
+  ngOnInit(): void {
+    this.getCategorySubscription = this.categoryService.getAllCategories()
+      .subscribe({
+        next: (response) => {
+          this.categories = response;
+        },
+        error: (error) => {
+          console.log('Error on Category list: ' + error)
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.getCategorySubscription?.unsubscribe();
+  }
 
 }
