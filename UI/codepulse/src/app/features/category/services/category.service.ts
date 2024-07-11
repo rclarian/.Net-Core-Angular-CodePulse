e36c5396516a5a9008f69ba01718f9e9e55fb2c6
Ryan Clarian <rclarian@gmail.com>
2024-07-11 11:10:30 +0800

    #42.Create UI Service method to GET all categories
    #42.Create UI Service method to GET all categories

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -3,6 +3,7 @@ import { AddCategoryRequest } from '../models/add-category-request-model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,15 @@ import { environment } from '../../../../environments/environment';
 //#37.Create Angular Services
 export class CategoryService {
 
-  url: string = environment.apiBaseUrl + '/categories';
+  url: string = environment.apiBaseUrl + '/Categories';
   constructor(private http: HttpClient) { }
 
   addCategory(model: AddCategoryRequest): Observable<void>{
     return this.http.post<void>(this.url, model);
+  }
+
+  getAllCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>(this.url);
   }
 
 }
