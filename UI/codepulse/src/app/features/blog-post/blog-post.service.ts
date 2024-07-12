3c1dacdccdb87ece8007ea60a12641bf05009b6f
Ryan Clarian <rclarian@gmail.com>
2024-07-12 15:40:03 +0800

    #60. UI - Create Blog Post Service
    #60. UI - Create Blog Post Service

---
+++ UI/codepulse/src/app/features/blog-post/blog-post.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AddBlogPost } from './models/add-blog-post.model';
+import { BlogPost } from './models/blog-post.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  url: string = environment.apiBaseUrl;
+  constructor(private http: HttpClient) { }
+
+  createBlogPost(data: AddBlogPost) : Observable<BlogPost>{
+    return this.http.post<BlogPost>(`${this.url}/api/BlogPosts`, data);
+  }
+}
