a366b83f35a1fabc13d46d442390989dffb26cb7
Ryan Clarian <rclarian@gmail.com>
2024-07-15 19:57:41 +0800

    #108. API - Testing Authorize Attribute - Error on using multiple DbContext
    #108. API - Testing Authorize Attribute - Error on using multiple DbContext

--- API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
+++ API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace CodePulse.API.Data
     //#21. Create DbContext Class
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
