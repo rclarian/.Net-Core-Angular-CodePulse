a25881b0668617a3c05398afa9f55977122b80dc
Ryan Clarian <rclarian@gmail.com>
2024-07-16 21:58:14 +0800

    #118. UI - Store JWT Token
    #118. UI - Store JWT Token

--- UI/codepulse/src/app/features/auth/login/login.component.ts
+++ UI/codepulse/src/app/features/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy } from '@angular/core';
 import { LoginRequest } from '../models/login-request.model';
 import { AuthService } from '../services/auth.service';
 import { Subscription } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +15,7 @@ export class LoginComponent implements OnDestroy{
   model: LoginRequest;
   authSubscription?: Subscription;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private cookieService: CookieService, private router: Router) {
     this.model = {
       email: '',
       password: ''
@@ -24,7 +26,11 @@ export class LoginComponent implements OnDestroy{
     this.authSubscription = this.authService.login(this.model)
       .subscribe({
         next: (res) => {
-          console.log(res);
+          //Set Auth Cookie
+          this.cookieService.set('Authorization', `Bearer ${res.token}`, undefined, '/', undefined, true, 'Strict');
+
+          //Redirect back to Home page
+          this.router.navigateByUrl('/');
         },
         error: (err) => {
           console.log('Error on Login: ' + err);
