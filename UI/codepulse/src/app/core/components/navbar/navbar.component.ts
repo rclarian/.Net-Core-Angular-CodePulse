51f53182c6e6fc4c3b3ae5cd46441618d8ced4f6
Ryan Clarian <rclarian@gmail.com>
2024-07-17 09:12:41 +0800

    #120. UI - Implement Logout
    #120. UI - Implement Logout

--- UI/codepulse/src/app/core/components/navbar/navbar.component.ts
+++ UI/codepulse/src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../../../features/auth/services/auth.service';
 import { Subscription } from 'rxjs';
+import { User } from '../../../features/auth/models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,20 +11,28 @@ import { Subscription } from 'rxjs';
 })
 export class NavbarComponent implements OnInit, OnDestroy{
   
+  user?: User;
   navbarSubscription?: Subscription;
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router){}
   
   ngOnInit(): void {
     this.navbarSubscription = this.authService.user()
       .subscribe({
         next: (res) => {
-          console.log(res);
+          this.user = res;
         },
         error: (err) => {
           console.log('Error navbar user: ' + err);
         }
       });
+    
+      this.user = this.authService.getUser();
+  }
+
+  onLogout(): void{
+    this.authService.logout();
+    this.router.navigateByUrl("/login");
   }
 
   ngOnDestroy(): void {
--- UI/codepulse/src/app/features/auth/services/auth.service.ts
+++ UI/codepulse/src/app/features/auth/services/auth.service.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { LoginResponse } from '../models/login-response.model';
 import { environment } from '../../../../environments/environment';
 import { User } from '../models/user.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,23 +14,46 @@ export class AuthService {
 
   $user = new BehaviorSubject<User | undefined>(undefined);
   url: string = environment.apiBaseUrl;
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
 
   login(request: LoginRequest): Observable<LoginResponse>{
     return this.http.post<LoginResponse>(`${this.url}/api/auth/login`, {
-      email: request.email,
-      password: request.password
+      email: request?.email,
+      password: request?.password
     });
   }
 
   setUser(user: User): void {
     this.$user.next(user);
-    localStorage.setItem('user-email', user.email);
-    localStorage.setItem('user-roles', user.roles.join(','));
+    localStorage?.setItem('user-email', user.email);
+    localStorage?.setItem('user-roles', user.roles.join(','));
   }
 
   user() : Observable<User | undefined> {
     return this.$user.asObservable();
+  }
+
+  getUser():User | undefined {
+    if (typeof window !== 'undefined' && window.localStorage){
+      const email = localStorage?.getItem('user-email');
+      const roles = localStorage?.getItem('user-roles');
+
+      if(email && roles) {
+        const user: User = {
+          email: email,
+          roles: roles?.split(',')
+        };
+        return user;
+      }
+    }
+    return undefined;
+  }
+
+  logout(): void{
+    localStorage?.clear();
+    this.cookieService?.delete('Authorization', '/');
+    this.$user?.next(undefined);
   }
 
 }
