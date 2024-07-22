804f59a4ae45a9e220e42076ba966880d20e1c4e
Ryan Clarian <rclarian@gmail.com>
2024-07-22 10:50:01 +0800

    #143. API - Creating student repository - Repository pattern
    #143. API - Creating student repository - Repository pattern

---
+++ API/StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using StudentAdminPortal.API.DataModels;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext _dbContext;
+
+        public SqlStudentRepository(StudentAdminContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public List<Student> GetStudents()
+        {
+            return _dbContext.Student.ToList();
+        }
+    }
+}
