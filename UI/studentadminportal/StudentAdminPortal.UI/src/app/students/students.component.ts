74d293021ab37a5e49d5b3546a01b1d315c7a00c
Ryan Clarian <rclarian@gmail.com>
2024-07-23 19:06:25 +0800

    #168. UI - Implementing Sorting on the Table
    #168. UI - Implementing Sorting on the Table

--- UI/studentadminportal/StudentAdminPortal.UI/src/app/students/students.component.ts
+++ UI/studentadminportal/StudentAdminPortal.UI/src/app/students/students.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { Student } from '../models/ui-models/student.model';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-students',
@@ -16,7 +17,8 @@ export class StudentsComponent implements OnInit, OnDestroy, AfterViewInit{
   students: Student[] = [];
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
-  @ViewChild(MatPaginator) matPaginator?: MatPaginator;
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
 
   constructor(private studentService: StudentService) {
 
@@ -38,6 +40,7 @@ export class StudentsComponent implements OnInit, OnDestroy, AfterViewInit{
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.matPaginator;
+    this.dataSource.sort = this.matSort;
   }
 
   ngOnDestroy(): void {
