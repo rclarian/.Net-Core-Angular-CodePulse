82e61e5bbab5b6e18263730c3af453355028b57f
Ryan Clarian <rclarian@gmail.com>
2024-07-24 09:23:53 +0800

    #177. Fetching Data of single student from API into UI
    #177. Fetching Data of single student from API into UI

--- UI/studentadminportal/StudentAdminPortal.UI/src/app/students/view-student/view-student.component.ts
+++ UI/studentadminportal/StudentAdminPortal.UI/src/app/students/view-student/view-student.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { StudentService } from '../student.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-view-student',
   templateUrl: './view-student.component.html',
   styleUrl: './view-student.component.css'
 })
-export class ViewStudentComponent {
+export class ViewStudentComponent implements OnInit, OnDestroy {
+
+  paramsSubscription?: Subscription = new Subscription();
+  studentSubscription?: Subscription = new Subscription();
+  studentId?: string | null;
+  constructor(private studentService: StudentService, private readonly route: ActivatedRoute){}
+  
+  ngOnInit(): void {
+    this.paramsSubscription = this.route.paramMap
+    .subscribe({
+      next: (params) => {
+        this.studentId = params.get('id');
+
+        if(this.studentId){
+          this.studentSubscription = this.studentService.getStudent(this.studentId)
+            .subscribe({
+              next: (res) => {
+                console.log(res)
+              },
+              error: (err) => {
+                console.log('Error on getStudent' + err);
+              }
+            });
+        }
+
+      },
+      error: (err) => {
+        console.log('Error on paramMap' + err);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+    this.studentSubscription?.unsubscribe();
+  }
+
 
 }
