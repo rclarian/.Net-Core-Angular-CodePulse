ef8756bb61a74bea170688cb47d57e930af915f3
Ryan Clarian <rclarian@gmail.com>
2024-07-25 20:43:03 +0800

    #202. UI - Creating Service Method
    #202. UI - Creating Service Method

--- UI/studentadminportal/StudentAdminPortal.UI/src/app/students/student.service.ts
+++ UI/studentadminportal/StudentAdminPortal.UI/src/app/students/student.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student } from '../models/api-models/student.model';
 import { UpdateStudentRequest } from '../models/api-models/update-student-request.model';
+import { AddStudentRequest } from '../models/api-models/add-student-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -39,6 +40,20 @@ export class StudentService {
 
   deleteStudent(studentId:string): Observable<Student>{
     return this.httpClient.delete<Student>(`${this.baseApiUrl}/students/${studentId}`);
+  }
+
+  addStudent(studentRequest: Student): Observable<Student>{
+    const addStudentRequest: AddStudentRequest = {
+      firstName: studentRequest.firstName,
+      lastName: studentRequest.lastName,
+      dateOfBirth: studentRequest.dateOfBirth,
+      email: studentRequest.email,
+      mobile: studentRequest.mobile,
+      genderId: studentRequest.genderId,
+      physicalAddress: studentRequest.address.physicalAddress,
+      postalAddress: studentRequest.address.postalAddress
+    }
+    return this.httpClient.post<Student>(`${this.baseApiUrl}/students/add`, addStudentRequest);
   }
 
 }
