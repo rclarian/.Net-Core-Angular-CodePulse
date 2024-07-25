1ecf41bd5584217f2cdb7aba5dc7efec99ed33af
Ryan Clarian <rclarian@gmail.com>
2024-07-25 10:26:29 +0800

    #187. UI - Show notification on update
    #187. UI - Show notification on update

--- UI/studentadminportal/StudentAdminPortal.UI/src/app/students/view-student/view-student.component.ts
+++ UI/studentadminportal/StudentAdminPortal.UI/src/app/students/view-student/view-student.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { Student } from '../../models/ui-models/student.model';
 import { GenderService } from '../../services/gender.service';
 import { Gender } from '../../models/ui-models/gender.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-view-student',
@@ -44,7 +45,8 @@ export class ViewStudentComponent implements OnInit, OnDestroy {
     private studentService: StudentService, 
     private readonly route: ActivatedRoute, 
     private genderService: GenderService,
-    private router: Router){}
+    private router: Router,
+    private snackbar: MatSnackBar){}
   
   ngOnInit(): void {
     this.paramsSubscription = this.route.paramMap
@@ -85,7 +87,9 @@ export class ViewStudentComponent implements OnInit, OnDestroy {
     this.updateStudentSubscription = this.studentService.updateStudent(this.studentId, this.student)
       .subscribe({
         next: (res) => {
-          console.log(res);
+          this.snackbar.open('Student updated successfull', undefined, {
+            duration: 2000
+          });
           this.router?.navigateByUrl('/students');
         },
         error: (err) => {
